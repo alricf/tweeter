32d5d5678e92f08fe58ce0e14acb14948dc3d335
ACF <alricfernandes11@gmail.com>
2022-11-13 12:51:10 -0500

    Fetching tweets with AJAX and displaying time passed since a tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,34 +3,36 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from initial-tweets.json
-$(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
+$(document).ready(function() {
+  // Fake data taken from initial-tweets.json
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
+
+  // Creating and Rendering Tweets Code
   const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
@@ -55,7 +57,7 @@ $(document).ready(function() {
         <p>${tweetData.content.text}</p>
       </section>
       <footer>
-        <p>${tweetData.created_at}</p>
+        <p>${timeago.format(tweetData.created_at)}</p>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -66,17 +68,26 @@ $(document).ready(function() {
 `);
     return $tweet;
   };
-  renderTweets(data);
+  // renderTweets(data);
 
-  $(".container form").submit(function (event) {
+  // Posting Tweets
+  $(".container form").submit(function(event) {
     event.preventDefault();
     const formData = $(this).serialize();
-    
+
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: formData,
-    })
+    });
   });
+
+  // Fetching Tweets
+  const loadTweets = () => {
+    $.get("/tweets", function(data) {
+      renderTweets(data);
+    });
+  };
+  loadTweets();
 
 });
